8b50fe9d0293afa4beaf026e08bf39eee53b0552
Laszlo Szikszai <sziklaszlo@gmail.com>
2018-04-28 10:52:08 +0200

    Added code

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# list-diff
-TBD
+# diffter
+
+**diffter** can be used to determine the difference between two list of any items and generate an HTML report about the results.
+
+## Prerequisites
+
+* NodeJS 6+
+
+## Usage
+
+The following script generated the report can be seen below:
+
+```javascript
+const {diff, saveReport} = require('diffter');
+
+const baseList = [1, 2, 3, 4];
+const subjectList = [1, 3, 4, 5];
+
+const results = diff(baseList, subjectList);
+saveReport('./report', results);
+```
+
+![report](example.png)
+
+The items are colored in the following way:
+
+* **Green** items are the **same** in both list.
+* **Blue** items are **changed position** in subject list.
+* **Red** items in base list are those which are **deleted** in subject list.
+* **Yellow** items in subject list are those which are **new** compared to the base list.
+* **Gray** items are the **ignored** items.
+
+## API
+
+### `diff(baseList, subjectList[, options])`
+
+**Params**:
+* `{Array} baseList` - the list of items used as the base list
+* `{Array} subjectList` - the list of subject items which are compared to the base list
+* `{DiffOptions} options` - options could be passed to the analyzer.
+
+**Returns**: `{DiffResults}` the results of the analysis
---
+++ test/diff.spec.js
@@ -0,0 +1,225 @@
+'use strict';
+
+const diff = require('../lib/diff');
+const {NOT_FOUND, IGNORED} = require('../lib/const');
+const {expect} = require('chai');
+const eql = require('deep-eql');
+
+describe('Create Diff', () => {
+    it('should work with empty lists', () => {
+        expect(diff([], [])).to.eql({
+            baseList: [],
+            subjectList: [],
+            indexes: []
+        });
+    });
+
+    it('should identify identical lists', () => {
+        const baseList = [1, 2, 3];
+        const subjectList = [1, 2, 3];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 1],
+                [2, 2]
+            ]
+        });
+    });
+
+    it('should identify new elements', () => {
+        const baseList = [1, 2, 3];
+        const subjectList = [1, 2, 3, 4];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 1],
+                [2, 2],
+                [NOT_FOUND, 3]
+            ]
+        });
+    });
+
+    it('should identify deleted elements', () => {
+        const baseList = [1, 2, 3, 4];
+        const subjectList = [1, 3, 4];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, NOT_FOUND],
+                [2, 1],
+                [3, 2]
+            ]
+        });
+    });
+
+    it('should identify elements in wrong order', () => {
+        const baseList = [1, 4, 3, 2];
+        const subjectList = [1, 3, 2, 4];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 3],
+                [2, 1],
+                [3, 2]
+            ]
+        });
+    });
+
+    it('should work with objects too', () => {
+        const baseList = [
+            {a: 1}, 
+            {a: 4}, 
+            {b: 1},
+            {a: 3}, 
+            {a: 2}
+        ];
+        const subjectList = [
+            {a: 1}, 
+            {a: 3},
+            {c: 1}, 
+            {a: 2}, 
+            {a: 4}
+        ];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 4],
+                [2, NOT_FOUND],
+                [3, 1],
+                [4, 3],
+                [NOT_FOUND, 2]
+            ]
+        });
+    });
+
+    it('should identify duplicate in base as deleted', () => {
+        const baseList = [1, 2, 2, 3];
+        const subjectList = [1, 2, 3];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 1],
+                [2, NOT_FOUND],
+                [3, 2]
+            ]
+        });
+    });
+
+    it('should identify duplicate in subject as new', () => {
+        const baseList = [1, 2, 3];
+        const subjectList = [1, 2, 2, 3];
+        expect(diff(baseList, subjectList)).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 1],
+                [2, 3],
+                [NOT_FOUND, 2]
+            ]
+        });
+    });
+
+    it('should handle custom comparator function', () => {
+        const baseList = [
+            {a: 1}, 
+            {a: 4},
+            {a: 3}, 
+            {b: 1}, 
+            {a: 2}
+        ];
+        const subjectList = [
+            {a: 1},
+            {c: 1}, 
+            {a: 3}, 
+            {a: 2}, 
+            {a: 4}
+        ];
+        const comparator = (o1, o2) => (o1.a || o2.a) ? o1.a === o2.a : eql(o1, o2);
+        expect(diff(baseList, subjectList, {
+            comparator
+        })).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 4],
+                [2, 2],
+                [3, NOT_FOUND],
+                [4, 3],
+                [NOT_FOUND, 1]
+            ]
+        });
+    });
+    
+    it('should handle custom filter function', () => {
+        const baseList = [
+            {a: 1}, 
+            {a: 4},
+            {a: 3}, 
+            {b: 1}, 
+            {a: 2}
+        ];
+        const subjectList = [
+            {a: 1},
+            {c: 1}, 
+            {a: 3}, 
+            {a: 2}, 
+            {a: 4}
+        ];
+        const filter = e => e.a;
+        expect(diff(baseList, subjectList, {
+            filter
+        })).to.eql({
+            baseList, subjectList,
+            indexes: [
+                [0, 0],
+                [1, 4],
+                [2, 2],
+                [3, IGNORED],
+                [4, 3],
+                [IGNORED, 1]
+            ]
+        });
+    });
+
+    it('should handle custom transform function', () => {
+        const baseList = [
+            {a: 1}, 
+            {a: 4},
+            {a: 3}, 
+            {b: 1}, 
+            {a: 2}
+        ];
+        const subjectList = [
+            {a: 1},
+            {c: 1}, 
+            {a: 3}, 
+            {a: 2}, 
+            {a: 4}
+        ];
+        const transform = e => JSON.stringify(e);
+        expect(diff(
+            baseList, 
+            subjectList, 
+            {
+                transform
+            }
+        )).to.eql({
+            baseList: baseList.map(transform),
+            subjectList: subjectList.map(transform),
+            indexes: [
+                [0, 0],
+                [1, 4],
+                [2, 2],
+                [3, NOT_FOUND],
+                [4, 3],
+                [NOT_FOUND, 1]
+            ]
+        });
+    });
+});
